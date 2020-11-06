e63e58c27b9132c535138909b1cae348717a6281
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2020-11-05 18:02:04 -0800

    Enemy1

---
+++ Assets/Scripts/OctopusBehavior.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+/*
+Name: Evan Anderson
+Date: 11/5/2020
+Desc: Basic Enemy (Octopus) only moves from left to right and dashes forward and up every once in a while
+*/
+[RequireComponent(typeof(Rigidbody2D))]
+public class OctopusBehavior : MonoBehaviour
+{
+    //NOT READY, SAVING FOR LATER
+    Rigidbody2D octopusRB;
+    public int DebugDashSpeed = 10;
+    public float DebugDashFrequency = 10;
+    public float DebugDashTime = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        throw new NotImplementedException();
+        octopusRB = GetComponent<Rigidbody2D>();
+        StartCoroutine(Dash());
+    }
+
+ 
+    IEnumerator Dash()
+    {
+        yield return new WaitForSeconds(.5f);
+        octopusRB.AddForce(new Vector2(-DebugDashSpeed, DebugDashSpeed));
+        yield return new WaitForSeconds(.5f);
+        //octopusRB.AddForce(new Vector2(DebugDashSpeed));
+        
+        StartCoroutine(Dash());
+    }
+
+}
---
+++ Assets/Scripts/SharkBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+Name: Evan Anderson
+Date: 11/5/2020
+Desc: Basic Enemy (Shark) only moves from left to right
+*/
+[RequireComponent(typeof(Rigidbody2D))]
+public class SharkBehaviour : MonoBehaviour
+{
+
+    Rigidbody2D sharkRB;
+    [Tooltip("The movement speed")]
+    public float Speed;
+    // Use this for initialization
+    void Start()
+    {
+        sharkRB = GetComponent<Rigidbody2D>();
+        sharkRB.AddForce(new Vector2(-Speed, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
