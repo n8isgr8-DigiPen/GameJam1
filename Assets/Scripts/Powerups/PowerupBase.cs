bec07d10163cfc0fc1139cf7a35ce8dd1949bda0
nathan stern <nathan.stern@digipen.edu>
2020-11-06 16:44:58 -0800

    fixed meta and moved files
    nice

--- Assets/Scripts/Powerups/PowerupBase.cs
+++ Assets/Scripts/Powerups/PowerupBase.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+﻿/*
+Name: Nate Stern
+Date: 11/6/20
+Desc: Base Class all temporary powerups go through
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class PowerupBase : MonoBehaviour
 {
+    //method every powerup requires
     public abstract void Pickup();
     
 }
