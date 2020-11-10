78385d6ca91b6d3f06415d470b53e3a33049097b
nate stern <nathan.stern@digipen.edu>
2020-11-09 18:34:35 -0800

    added quit functionality and official game scene
    ok

---
+++ Assets/Scripts/GameMenuHelper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenuHelper : MonoBehaviour
+{
+    bool paused = false;
+    GameObject quitButton;
+    GameObject pauseText;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    private void Start()
+    {
+        pauseText = GameObject.Find("Pause Text");
+        quitButton = GameObject.Find("Quit Button");
+        quitButton.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    private void Pause()
+    {
+        Debug.Log("Pause");
+        paused = !paused;
+        Time.timeScale = paused ? 0 : 1;
+        quitButton.SetActive(paused);
+        pauseText.SetActive(!paused);
+    }
+}
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+
+#endif    
+
     }
 }
