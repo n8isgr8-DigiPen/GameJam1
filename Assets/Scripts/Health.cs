418714ad990d7b56b32cf9c0be869f47b61da54a
EricLighthall <eric.lighthall@digipen.edu>
2020-11-12 14:47:39 -0800

    Health Bar Added
    Game now displays player health at the top left.

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -11,18 +11,32 @@ public class Health : MonoBehaviour
 {
     public UnityEvent onDeath = new UnityEvent();
     private GameObject waveSpawner;
+    private HealthBar healthBar;
 
     private void Start()
     {
         waveSpawner = GameObject.Find("WaveManager");
         if (tag != "Player") return;
         PlayerManager.PlayerHealth = this;
+        if(tag == "Player")
+        {
+            healthBar = GameObject.Find("HealthBar").GetComponent<HealthBar>();
+        }
     }
 
     public int HEALTH = 50;
+    public int MaxHealth = 50;
     public void updateHealth(int healthChange) 
     {
         HEALTH += healthChange;
+        if(HEALTH > MaxHealth)
+        {
+            HEALTH = MaxHealth;
+        }
+        if(tag == "Player")
+        {
+            healthBar.SetSize((float)HEALTH/50);
+        }
         //If enemy dies, remove from list and destroy object.
         if(HEALTH <= 0 && gameObject.tag == "Enemy")
         {
@@ -33,6 +47,7 @@ public class Health : MonoBehaviour
         //Else, destroy object on death.
         else if(HEALTH <= 0)
         {
+            healthBar.SetSize(0);
             onDeath.Invoke();
             Destroy(gameObject);
         }
