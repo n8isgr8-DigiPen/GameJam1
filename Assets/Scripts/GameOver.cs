cdf8c452939fafd2ed9ab5a74c4d2237a61557f8
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2020-11-12 16:29:35 -0800

    Update GameOver.cs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/*
+Name: Evan Anderson
+Date: 11/12/2020
+Desc: Game Over
+*/
 public class GameOver : MonoBehaviour
 {
+    [Tooltip("What Gameobject in the Hirearchy is the end panel")]
     public GameObject EndScreen;
     private void Start()
     {
