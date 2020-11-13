54af4907e717ff3ce2026735114657e414d57186
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2020-11-12 16:28:52 -0800

    Adds end panel

---
+++ Assets/Scripts/EndButtons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+Name: Evan Anderson
+Date: 11/12/2020
+Desc: Control End Buttons
+*/
+public class EndButtons : MonoBehaviour
+{
+    
+    public void menu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject EndScreen;
+    private void Start()
+    {
+        Time.timeScale = 1;
+        
+    }
+    
+    public void triggerMe()
+    {
+        EndScreen.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
