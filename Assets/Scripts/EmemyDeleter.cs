94b747b1877b547c7774a24ff4a5cb2e4d174f29
evanAndersonDigipen <71675591+evanAndersonDigipen@users.noreply.github.com>
2020-11-13 16:18:33 -0800

    Added Enemy Destroyer

---
+++ Assets/Scripts/EmemyDeleter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmemyDeleter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject, 3);
+    }
+}
+
